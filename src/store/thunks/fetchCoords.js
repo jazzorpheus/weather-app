4f9dd972f67198548741a199e86a84e0e8bd084e
James Redgate <redgate888@hotmail.com>
2024-04-14 14:25:50 +0100

    Add checklist and remove commented out code in thunks

--- src/store/thunks/fetchCoords.js
+++ src/store/thunks/fetchCoords.js
@@ -33,63 +33,6 @@ const fetchCoords = createAsyncThunk("/fetchCoords", async (searchTerm) => {
     alert("Invalid search term, defaulting to Manchester, UK.");
     return [-2.245115, 53.479489];
   }
-
-  // // *******************************************************
-  // // ******************* IN DEVELOPMENT ********************
-  // // *******************************************************
-  // if (process.env.NODE_ENV !== "production") {
-  //   const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
-  //   const geocoder = mbxGeocoding({ accessToken: MAPBOX_TOKEN });
-  //   const response = await geocoder
-  //     .forwardGeocode({
-  //       query: searchTerm,
-  //       limit: 1,
-  //     })
-  //     .send();
-  //   if (
-  //     response &&
-  //     response.body &&
-  //     response.body.features &&
-  //     response.body.features.length > 0
-  //   ) {
-  //     console.log(
-  //       "fetchCoords response data:",
-  //       response.body.features[0].geometry.coordinates
-  //     );
-  //     return response.body.features[0].geometry.coordinates;
-  //     // *******************************************************
-  //     // ******************* IN PRODUCTION ********************
-  //     // *******************************************************
-  //   } else {
-  //     console.error("Invalid search term, defaulting to Manchester, UK.");
-  //     alert("Invalid search term, defaulting to Manchester, UK.");
-  //     return [-2.245115, 53.479489];
-  //   }
-  // } else {
-  //   const geocoder = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
-  //   const response = await geocoder
-  //     .forwardGeocode({
-  //       query: searchTerm,
-  //       limit: 1,
-  //     })
-  //     .send();
-  //   if (
-  //     response &&
-  //     response.body &&
-  //     response.body.features &&
-  //     response.body.features.length > 0
-  //   ) {
-  //     console.log(
-  //       "fetchCoords response data:",
-  //       response.body.features[0].geometry.coordinates
-  //     );
-  //     return response.body.features[0].geometry.coordinates;
-  //   } else {
-  //     console.error("Invalid search term, defaulting to Manchester, UK.");
-  //     alert("Invalid search term, defaulting to Manchester, UK.");
-  //     return [-2.245115, 53.479489];
-  //   }
-  // }
 });
 
 export { fetchCoords };
--- src/store/thunks/fetchWeatherData.js
+++ src/store/thunks/fetchWeatherData.js
@@ -13,27 +13,6 @@ const fetchWeatherData = createAsyncThunk(
     console.log("fetchWeatherData response data:", response.data);
 
     return response.data;
-
-    // // *******************************************************
-    // // ******************* IN DEVELOPMENT ********************
-    // // *******************************************************
-    // if (process.env.NODE_ENV !== "production") {
-    //   const WEATHER_KEY = import.meta.env.VITE_WEATHER_KEY;
-    //   const response = await axios.get(
-    //     `https://api.openweathermap.org/data/2.5/weather?lat=${coords[1]}&lon=${coords[0]}&appid=${WEATHER_KEY}`
-    //   );
-    //   console.log("fetchWeatherData response data:", response.data);
-
-    //   return response.data;
-    //   // *******************************************************
-    //   // ******************* IN PRODUCTION ********************
-    //   // *******************************************************
-    // } else {
-    //   const response = await axios.get(
-    //     `https://api.openweathermap.org/data/2.5/weather?lat=${coords[1]}&lon=${coords[0]}&appid=${process.env.WEATHER_KEY}`
-    //   );
-    //   return response.data;
-    // }
   }
 );
 
