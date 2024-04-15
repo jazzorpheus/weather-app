42c9a12d0e71cc4802298b25d023bfddb3884ace
James Redgate <redgate888@hotmail.com>
2024-04-15 11:26:33 +0100

    extract env var

--- src/store/thunks/fetchWeatherData.js
+++ src/store/thunks/fetchWeatherData.js
@@ -6,8 +6,12 @@ const fetchWeatherData = createAsyncThunk(
   async (coords) => {
     console.log("IN FETCH WEATHER DATA");
 
+    console.log(process.env.WEATHER_KEY);
+    const WEATHER_KEY = process.env.WEATHER_KEY;
+    console.log(WEATHER_KEY);
+
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${coords[1]}&lon=${coords[0]}&appid=${process.env.WEATHER_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${coords[1]}&lon=${coords[0]}&appid=${WEATHER_KEY}`
     );
 
     console.log("fetchWeatherData response data:", response.data);
