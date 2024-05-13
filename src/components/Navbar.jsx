6c67e76b35b23f72e5b20e52d52d0abbdbcd0320
Jazz Orpheus <redgate888@hotmail.com>
2024-05-13 14:26:46 +0100

    homemade navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,28 +9,36 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ArchiveIcon from "@mui/icons-material/Archive";
 import Paper from "@mui/material/Paper";
 
-export default function FixedBottomNavigation() {
-  const [value, setValue] = useState(0);
-  const ref = useRef(null);
+export default function Navbar() {
 
   return (
-    <Box sx={{ pb: 7 }} ref={ref}>
-      <Paper
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-        elevation={3}
-      >
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        >
-          <BottomNavigationAction label="Current" icon={<RestoreIcon />} />
-          <BottomNavigationAction label="Map" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Forecast" icon={<ArchiveIcon />} />
-        </BottomNavigation>
-      </Paper>
-    </Box>
-  );
+    <footer className="fixed bottom-0 w-screen p-5 bg-gradient-to-t from-blue-950 to-blue-500">
+      <nav className="flex justify-around items-center">
+          <div>Current</div>
+          <div>Map</div>
+          <div>Forecast</div>
+      </nav>
+    </footer>
+  )
+
+  // return (
+  //   <Box sx={{ pb: 7 }} ref={ref}>
+  //     <Paper
+  //       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+  //       elevation={3}
+  //     >
+  //       <BottomNavigation
+  //         showLabels
+  //         value={value}
+  //         onChange={(event, newValue) => {
+  //           setValue(newValue);
+  //         }}
+  //       >
+  //         <BottomNavigationAction label="Current" icon={<RestoreIcon />} />
+  //         <BottomNavigationAction label="Map" icon={<FavoriteIcon />} />
+  //         <BottomNavigationAction label="Forecast" icon={<ArchiveIcon />} />
+  //       </BottomNavigation>
+  //     </Paper>
+  //   </Box>
+  // );
 }
