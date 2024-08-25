1bfa8674326d100812b0e387eadfb836ef0efabf
James Redgate <redgate888@hotmail.com>
2024-08-25 02:17:32 +0100

    add opaque background to icon row in forecast table

--- src/components/IconCell.jsx
+++ src/components/IconCell.jsx
@@ -5,5 +5,9 @@ export default function IconCell({ dataPoint }) {
     dataPoint.weather[0].main,
     dataPoint.weather[0].description
   );
-  return <td className="px-4 py-3 border-none">{icon}</td>;
+  return (
+    <td className="px-5 py-3 border-none weather-stat bg-[rgba(23,37,84,0.4)]">
+      {icon}
+    </td>
+  );
 }
