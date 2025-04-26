3c055d212b1430d3fe52cf696c875d9b6c074013
James Redgate <redgate888@hotmail.com>
2025-04-26 16:22:28 +0100

    fix custom marker overflow

--- src/components/CustomMarker.jsx
+++ src/components/CustomMarker.jsx
@@ -1,17 +1,15 @@
 export default function CustomMarker({ data, icon }) {
   return (
     <div className="custom-marker-container">
-      <div className="custom-marker"></div>
+      <div className="custom-marker-pointer"></div> {/* Pointer first! */}
       {data ? (
-        <div className="custom-marker-text flex flex-col items-center">
-          <div className="scale-75">{icon}</div>
-          <span className="relative bottom-1">{data.name}</span>
-          <span className="relative bottom-1">
-            {data.main.temp.toFixed(1)}°C
+        <div className="custom-marker-text flex flex-col items-center gap-1">
+          <span>{icon}</span>
+          <span>{data.main.temp.toFixed(1)}°C</span>
+          <span className="bg-white text-black w-[100%] rounded-md">
+            {data.name}
           </span>
-          <span className="relative bottom-1">
-            {data.weather[0].description}
-          </span>
+          <span>{data.weather[0].description}</span>
         </div>
       ) : (
         "Loading data..."
