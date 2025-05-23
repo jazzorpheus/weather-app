0d7fe60ba52c75559973d58e6a0eddf67ae368a3
James Redgate <redgate888@hotmail.com>
2025-05-23 22:48:38 +0100

    compress landing images

--- src/pages/ForecastPage.jsx
+++ src/pages/ForecastPage.jsx
@@ -1,15 +1,11 @@
 // React
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useMemo } from "react";
 
 // React-Redux
 import { useSelector, useDispatch } from "react-redux";
 
 // Async Thunks
 import { fetchForecastWeather } from "../store";
-
-// Custom hooks
-import useGetBackground from "../hooks/use-get-background";
-import useGetIcon from "../hooks/use-get-icon";
 
 // Utilites
 import convertWeatherData from "../utils/data-conversions/convertWeatherData";
@@ -23,8 +19,7 @@ import { FaCircleArrowLeft } from "react-icons/fa6";
 import { FaCircleArrowRight } from "react-icons/fa6";
 import { TiLocationArrow } from "react-icons/ti";
 
-// Universally Unique Identifiers for <td> arrays
-import { v4 as uuid } from "uuid";
+// Local Components
 import IconCell from "../components/IconCell";
 
 export default function ForecastPage() {
@@ -36,21 +31,21 @@ export default function ForecastPage() {
 
   // State from store
   const coords = useSelector((state) => state.coords.coords);
-  const currentWeather = useSelector((state) => state.currentWeather);
   const forecastWeather = useSelector((state) => state.forecastWeather);
-
-  // if (forecastWeather && forecastWeather.data) {
-  //   console.log("FORECAST DATA:", forecastWeather.data);
-  // }
-
-  const tableContainerRef = useRef(null);
 
   useEffect(() => {
     dispatch(fetchForecastWeather(coords));
-  }, [coords]);
+  }, [coords, dispatch]);
 
-  let styles = "flex flex-col justify-center h-dvh w-dvw";
-  styles += useGetBackground();
+  // If no placename, use coords for heading
+  const latLng = (
+    <span>
+      Lat: {coords[1].toFixed(2)} | Lon: {coords[0].toFixed(2)}
+    </span>
+  );
+
+  // Table Ref
+  const tableContainerRef = useRef(null);
 
   const scrollLeft = () => {
     if (tableContainerRef.current) {
@@ -70,234 +65,208 @@ export default function ForecastPage() {
     }
   };
 
-  let foreLoadOrErr;
-  let timestamps = [];
-  let icons = [];
-  let temperature = [];
-  let feelsLike = [];
-  let clouds = [];
-  let windDirection = [];
-  let windSpeed = [];
-  let windGust = [];
-  let visibility = [];
-  let humidity = [];
-  let latLng;
+  const tableRowsByType = useMemo(() => {
+    if (!forecastWeather.data?.list) return null;
+
+    const list = forecastWeather.data.list;
+
+    return list.reduce(
+      (acc, timeslot) => {
+        const converted = convertWeatherData(timeslot);
+
+        const timeString = moment
+          .unix(timeslot.dt)
+          .tz(timezone)
+          .format("ddd HH:mm");
+
+        Object.keys(acc).forEach((key, idx) => {
+          if (key === "timestamps") {
+            acc.timestamps.push(
+              <td className="weather-stat bg-gray-900" key={timeslot.dt}>
+                {timeString}
+              </td>
+            );
+          } else if (key === "icons") {
+            acc.icons.push(<IconCell key={timeslot.dt} dataPoint={timeslot} />);
+          } else if (key === "temperature") {
+            const tempColor = tempToColor(converted[2].value, 0.6);
+            acc.temperature.push(
+              <td
+                style={{ backgroundColor: tempColor }}
+                className={"weather-stat py-2"}
+                key={timeslot.dt}
+              >
+                {converted[idx].value}
+                {converted[idx].units}
+              </td>
+            );
+          } else if (key === "feelsLike") {
+            const feelsLikeColor = tempToColor(converted[3].value, 0.6);
+            acc.feelsLike.push(
+              <td
+                style={{ backgroundColor: feelsLikeColor }}
+                className="weather-stat py-2"
+                key={timeslot.dt}
+              >
+                {converted[idx]["value"]}
+                {converted[idx]["units"]}
+              </td>
+            );
+          } else if (key === "windDirection") {
+            acc.windDirection.push(
+              <td className="weather-stat py-2" key={timeslot.dt}>
+                <TiLocationArrow className={converted[8].styles} />
+                {converted[8].value}
+              </td>
+            );
+          } else {
+            acc[key].push(
+              <td className="weather-stat py-2" key={timeslot.dt}>
+                {converted[idx]["value"]}
+                {converted[idx]["units"]}
+              </td>
+            );
+          }
+        });
+
+        return acc;
+      },
+      {
+        timestamps: [],
+        icons: [],
+        temperature: [],
+        feelsLike: [],
+        clouds: [],
+        humidity: [],
+        windSpeed: [],
+        windGust: [],
+        windDirection: [],
+        visibility: [],
+      }
+    );
+  }, [forecastWeather.data?.list, timezone]);
+
   if (forecastWeather.isLoading) {
-    styles += " items-center";
-    foreLoadOrErr = <p>Loading...</p>;
-  } else if (forecastWeather.error) {
-    styles += " items-center";
-    foreLoadOrErr = <p>ERROR: {forecastWeather.error.message}</p>;
-  } else {
-    // ***********************************************************************
-    const forecast = forecastWeather.data?.list; // || [];
-    forecast.forEach((item) => {
-      const convertedItem = convertWeatherData(item);
-      // console.log("ITEM:", convertedItem);
-      timestamps.push(
-        <td className="weather-stat bg-gray-900" key={uuid()}>
-          {moment.unix(item.dt).tz(timezone).format("ddd HH:mm")}
-        </td>
-      );
-      icons.push(<IconCell key={uuid()} dataPoint={item} />);
-      const tempColor = tempToColor(convertedItem[2].value, 0.6);
-      let tempStyle = {
-        backgroundColor: tempColor,
-      };
-      temperature.push(
-        <td style={tempStyle} className="weather-stat py-2" key={uuid()}>
-          {convertedItem[2].value}
-          {convertedItem[2].units}
-        </td>
-      );
-      const feelsLikeColor = tempToColor(convertedItem[2].value, 0.6);
-      let feelsLikeStyle = {
-        backgroundColor: feelsLikeColor,
-      };
-      feelsLike.push(
-        <td style={feelsLikeStyle} className="weather-stat py-2" key={uuid()}>
-          {convertedItem[3].value}
-          {convertedItem[3].units}
-        </td>
-      );
-      clouds.push(
-        <td className="weather-stat py-2" key={uuid()}>
-          {convertedItem[4].value}
-          {convertedItem[4].units}
-        </td>
-      );
-      humidity.push(
-        <td className="weather-stat py-2" key={uuid()}>
-          {convertedItem[5].value}
-          {convertedItem[5].units}
-        </td>
-      );
-      windSpeed.push(
-        <td className="weather-stat py-2" key={uuid()}>
-          {convertedItem[6].value}
-          {convertedItem[6].units}
-        </td>
-      );
-      windGust.push(
-        <td className="weather-stat py-2" key={uuid()}>
-          {convertedItem[7].value}
-          {convertedItem[7].units}
-        </td>
-      );
-      windDirection.push(
-        <td className="weather-stat py-2" key={uuid()}>
-          <TiLocationArrow className={convertedItem[8].styles} />
-          {convertedItem[8].value}
-        </td>
-      );
-      visibility.push(
-        <td className="weather-stat py-2" key={uuid()}>
-          {convertedItem[9].value}
-          {convertedItem[9].units}
-        </td>
-      );
-    });
-    latLng = (
-      <span>
-        Latitude: {coords[1].toFixed(2)} | Longitude: {coords[0].toFixed(2)}
-      </span>
+    return (
+      <div className="flex flex-col justify-center items-center h-dvh w-dvw  bg-gradient-to-t from-zinc-700 to-blue-400">
+        <p>Loading...</p>
+      </div>
     );
   }
 
-  const currentIcon = useGetIcon(currentWeather.data);
-  let currLoadOrErr;
-  if (currentWeather.isLoading) {
-    currLoadOrErr = <p>Loading...</p>;
-  } else if (currentWeather.error) {
-    currLoadOrErr = <p>ERROR: {currentWeather.error.message}</p>;
-  } else {
-    currLoadOrErr = false;
+  if (forecastWeather.error) {
+    return (
+      <div className="flex justify-center items-center h-dvh w-dvw bg-gradient-to-t from-zinc-700 to-blue-400">
+        <p>ERROR: {forecastWeather.error.message}</p>
+      </div>
+    );
   }
 
   return (
-    <div className={styles}>
+    <div className="flex justify-center h-dvh w-dvw bg-gradient-to-t from-zinc-700 to-blue-400">
       <button
         onClick={scrollLeft}
         className="fixed left-0 top-1/2 -translate-y-1/2 z-10 p-2"
       >
         <FaCircleArrowLeft className="scale-150" />
       </button>
-      {foreLoadOrErr ? (
-        foreLoadOrErr
-      ) : (
-        <div
-          className="table-container relative overflow-x-scroll"
-          ref={tableContainerRef}
-        >
-          <h1 className="text-3xl font-bold text-center sticky left-0">
-            {forecastWeather.data?.city?.name || latLng}
-          </h1>
-          <div className="flex justify-center sticky left-0">{currentIcon}</div>
-          <h2 className="text-2xl font-bold text-center sticky left-0 mb-2">
-            {currLoadOrErr
-              ? currLoadOrErr
-              : currentWeather.data.weather[0].description
-                  .charAt(0)
-                  .toUpperCase() +
-                  currentWeather.data.weather[0].description.slice(1) || latLng}
-          </h2>
-          <table className="rounded">
-            <tbody>
-              {/* <tr> */}
-              {/* <th */}
-              {/* className="arrow-right sticky left-0 bg-gradient-to-t from-zinc-900 to-blue-900 rounded" */}
-              {/* colSpan={2} */}
-              {/* > */}
-              {/* <span> */}
-              {/* <h2>Time</h2> */}
-              {/* </span> */}
-              {/* </th> */}
-              {/* </tr> */}
-              <tr className="text-center sticky top-0 z-10">{timestamps}</tr>
-              <tr className="text-center">{icons}</tr>
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2>Temperature</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{temperature}</tr>
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2>Feels like temperature</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{feelsLike}</tr>
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2>Cloud cover</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{clouds}</tr>
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2 className="opacity-100">Humidity</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{humidity}</tr>
+      {/* TABLE START */}
+      <div
+        className="table-container relative overflow-x-scroll"
+        ref={tableContainerRef}
+      >
+        <h1 className="text-3xl font-bold text-center sticky left-0 mb-5">
+          {forecastWeather.data?.city?.name || latLng}
+        </h1>
+        <table className="rounded">
+          <tbody>
+            <tr className="text-center sticky top-0 z-10">
+              {tableRowsByType.timestamps}
+            </tr>
+            <tr className="text-center">{tableRowsByType.icons}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2>Temperature</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.temperature}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2>Feels like temperature</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.feelsLike}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2>Cloud cover</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.clouds}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2 className="opacity-100">Humidity</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.humidity}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2>Wind speed</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.windSpeed}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2>Wind gust</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.windGust}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2>Wind direction</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.windDirection}</tr>
+            <tr>
+              <th
+                className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
+                colSpan={2}
+              >
+                <h2>Visibility</h2>
+              </th>
+            </tr>
+            <tr className="text-center">{tableRowsByType.visibility}</tr>
+          </tbody>
+        </table>
+      </div>
 
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2>Wind speed</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{windSpeed}</tr>
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2>Wind gust</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{windGust}</tr>
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2>Wind direction</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{windDirection}</tr>
-              <tr>
-                <th
-                  className="arrow-right sticky left-0 bg-gradient-to-r from-zinc-900 to-blue-800 rounded"
-                  colSpan={2}
-                >
-                  <h2>Visibility</h2>
-                </th>
-              </tr>
-              <tr className="text-center">{visibility}</tr>
-            </tbody>
-          </table>
-          <button
-            onClick={scrollRight}
-            className="fixed right-0 top-1/2 -translate-y-1/2 z-10 p-2"
-          >
-            <FaCircleArrowRight className="scale-150" />
-          </button>
-        </div>
-      )}
+      {/* TABLE END */}
+      <button
+        onClick={scrollRight}
+        className="fixed right-0 top-1/2 -translate-y-1/2 z-10 p-2"
+      >
+        <FaCircleArrowRight className="scale-150" />
+      </button>
     </div>
   );
 }
